8f1b6a53c99a2ff335cee44432ecdfcd3190a07c
astrorobot13 <astrorobot13@gmail.com>
2025-10-23 20:45:26 -0400

    Create cache.js

---
+++ script/cache.js
@@ -0,0 +1,13 @@
+async function cacheFirstWithRefresh(request) {
+  const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
+    if (networkResponse.ok) {
+      const cache = await caches.open("MyCache_1");
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  });
+  return (await caches.match(request)) || (await fetchResponsePromise);
+}
+self.addEventListener("fetch", (event) => {
+  event.respondWith(cacheFirstWithRefresh(event.request));
+});
