f532138534892a396e2b510fd7e76abf340f8849
astrorobot13 <astrorobot13@gmail.com>
2026-01-18 13:51:25 -0500

    Add files via upload

---
+++ editor/cache.js
@@ -0,0 +1,18 @@
+async function networkFirst(request) {
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open("MyCache_1");
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || Response.error();
+  }
+}
+self.addEventListener("fetch", (event) => {
+  if (event.request.method == "GET") {
+    event.respondWith(networkFirst(event.request));
+  }
+});
